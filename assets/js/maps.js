e3a587f3c3f61982bbfb9ae8bf55d23b282d4c7b
asplesa <reillys2004@gmail.com>
2021-02-08 14:38:58 +0000

    Add maps.js and 6 locations on map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,30 +1,88 @@
+ function initMap() {
+   var dub = { lat: 53.34965776701125, lng: -6.260817096501578 }; /*Dublin*/
+    var girifsc = { lat: 53.35214202715207, lng: -6.2462966290529245 }; /*Giraffe IFSC*/
+    var girhar = { lat: 53.336359672299054, lng: -6.262776120337468 }; /*Giraffe Harcourt Road*/
+    var rain = { lat: 53.37156473683785, lng: -6.241833532524138 }; /*Rainbow*/
+    var dumbo = { lat: 53.412061689267716, lng: -6.1587495296582615  }; /*Dumbo childminding*/
+    var cuddle = { lat: 53.280129998990056, lng: -6.253364781102455 }; /*Cuddle childminders*/
+    var pam = { lat: 53.26585083117924, lng: -6.219547516528554 }; /*Pamala childminder*/  
+       
+    if (document.getElementById("location").value === 'dub') {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 10,
+            center: dub
+        });
+        new google.maps.Marker({
+            position: dub,
+            map,
+        });
+    }
 
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
-                center: {
-                    lat: 46.619261,
-                    lng: -33.134766
-                }
-            });
-            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+    else if (document.getElementById("location").value === 'girifsc') {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: girifsc
+        });
+        new google.maps.Marker({
+            position: girifsc,
+            map,
+        });
+    }
 
-            var locations = [
-                {lat: 53.320980, lng: -6.234470}, 
-                {lat: 53.326550, lng: -6.255530}, 
-                {lat: 53.330680, lng: -6.229670}, 
-                {lat: 53.335098, lng: -6.215700},
-                {lat: 53.335098, lng: -6.215700}, 
-                {lat: 53.335098, lng: -6.215700},
-            ];
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
+    else if (document.getElementById("location").value === 'girhar') {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: girhar
+        });
+        new google.maps.Marker({
+            position: girhar,
+            map,
+        });
+    }
 
-            var markerCluster = new MarkerClusterer(map, markers, {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-            });
-        }
+
+else if (document.getElementById("location").value === 'rain') {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: rain
+        });
+        new google.maps.Marker({
+            position: rain,
+            map,
+        });
+    }
+
+    else if (document.getElementById("location").value === 'dumbo') {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: dumbo
+        });
+        new google.maps.Marker({
+            position: dumbo,
+            map,
+        });
+    }
+
+else if (document.getElementById("location").value === 'cuddle') {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: cuddle
+        });
+        new google.maps.Marker({
+            position: cuddle,
+            map,
+        });
+    }
+
+    else if (document.getElementById("location").value === 'pam') {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: pam
+        });
+        
+        new google.maps.Marker({
+            position: pam,
+            map,
+        });
+    }
+ }
