e908c6c081459e3fe1b8bd6e8d9c49e8e65ad3fc
asplesa <reillys2004@gmail.com>
2021-02-10 12:32:35 +0000

    Linking EmailJS to the website

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+  function sendMail(contactForm) {
+    emailjs.send("service_z4u9c3s","Baxters", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "from_phone": contactForm.phone.value,
+        "from_query": contactForm.query.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
